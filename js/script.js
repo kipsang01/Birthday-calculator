0d3db55ca8dc5dd5dfd17efb1273deeaff710934
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-04 23:14:42 +0300

    Take user input and store in variables

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 function calculateDay(){
-    
+        var dd = parseInt(document.getElementById('day').value);
+        var mm = parseInt(document.getElementById('month').value);
+        var year = document.getElementById('year').value;
 }

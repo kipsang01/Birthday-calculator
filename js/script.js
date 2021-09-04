8c063b54ea911c59813e5315bf06e3c63156c3f3
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-04 23:16:47 +0300

    Return calculations of user input

--- js/script.js
+++ js/script.js
@@ -4,4 +4,7 @@ function calculateDay(){
         var dd = parseInt(document.getElementById('day').value);
         var mm = parseInt(document.getElementById('month').value);
         var year = document.getElementById('year').value;
+        var cc = parseInt(year.slice(0, 2));
+        var yy = parseInt(year.slice(2, 4));
+        return Math.floor((((cc/4)-2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7);
 }

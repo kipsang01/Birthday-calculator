a007802aecb8f14cd2bb0b78e06e8fe597593c41
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-04 23:13:27 +0300

    Initialize function to calculate day

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+function calculateDay(){
+    
+}

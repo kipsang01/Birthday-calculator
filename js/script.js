6a41e54e066bbbf0da7f4d61c46ea7bb946f05ae
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-04 23:22:26 +0300

    Add conditional statements

--- js/script.js
+++ js/script.js
@@ -11,4 +11,49 @@ function calculateDay(){
 function displayName(){
     var gender = (document.getElementById('Gender')).value;
     var day = calculateDay();
+    if (day===0 && gender ==='male') {
+        document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[0];
+        }
+        else if (day===0 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[0];
+        }
+        else if (day===1 && gender==='male') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[1];
+        }
+        else if (day===1 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[1];
+        }
+        else if (day===2 && gender==='male') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[2];
+        }
+        else if (day===2 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[2];
+        }
+        else if (day===3 && gender==='male') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[3];
+        }
+        else if (day===3 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[3];
+        }
+        else if (day===4 && gender==='male') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[4];
+        }
+        else if (day===4 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[4];
+        }
+        else if (day===5 && gender==='male') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[5];
+        }
+        else if (day===5 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[5];
+        }
+        else if (day===6 && gender==='male') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[6];
+        }
+        else if (day===6 && gender==='female') {
+            document.getElementById('displayAkan').innerHTML = "Your Name is "+ femaleNames[6];
+        }
+        else{
+            document.getElementById('displayAkan').innerHTML = "invalid input";
+        }
 }

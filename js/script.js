ee3270400782e923e27bef7a814511861155fa15
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-09-05 17:49:59 +0300

    validate date inputs by users

--- js/script.js
+++ js/script.js
@@ -5,14 +5,29 @@ function calculateDay(){
         var mm = parseInt(document.getElementById('month').value);
         var year = document.getElementById('year').value;
         var cc = parseInt(year.slice(0, 2));
-        var yy = parseInt(year.slice(2, 4));
-        return Math.floor((((cc/4)-2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7);
+        var yy = parseInt(year.slice(2));
+        if (dd>0 && dd<32 && mm>0 && mm<13) {
+            return Math.floor((((cc/4)-2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd)%7);
+        } else {
+            alert("invalid date/month/year input")
+        }
+       
 }
 function genderName(){
-    
+    if (document.getElementById('male').checked) {
+        var gender = document.getElementById('male').value;
+        return gender;
+        }
+    else if (document.getElementById('female').checked) {
+         var gender = document.getElementById('female').value;
+         return gender;
+         }
+     else{
+         alert("please check the gender to see your name")
+     }
 }
 function displayName(){
-    var gender = (document.getElementById('Gender')).value;
+    var gender = genderName();
     var day = calculateDay();
     if (day===0 && gender ==='male') {
         document.getElementById('displayAkan').innerHTML = "Your Name is "+ maleNames[0];
